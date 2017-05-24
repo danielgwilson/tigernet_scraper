d9f44a43f853c2020d984326fcf74cfd9a29e83c
danielgwilson <daniel@danielgwilson.com>
2017-05-23 21:53:19 -0400

    Added virtualenv for better package management and preparation for deployment. Started flask interface (only hello world atm).

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -7,12 +7,12 @@ import csv
 import json
 import sys
 from getpass import getpass
+import pickle
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-import pickle
 from pymongo import MongoClient
 
 
